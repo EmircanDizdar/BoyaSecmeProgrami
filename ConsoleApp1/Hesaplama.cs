bdef6991899e1b3929a1af95d901d2b2b37544aa
edi <edi@ERCTR004>
2023-03-02 10:44:12 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp1/Hesaplama.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Hesaplama
+    {
+        private string[] renklerAdi = { "Beyaz", "Gri", "Kahverengi", "Mavi", "Mor", "Pembe", "Sari", "Siyah", "Yesil" };
+        private int[] renkFiyati = { 36, 36, 40, 37, 35, 35, 40, 40, 37 };
+        private int belirtec = 0;
+        private int adet = 0;
+        private int secenek = 0;
+        private string tanim = "";
+        private bool whileDongu = true;
+        private bool switchDongu = true;
+        public int[] renkEkleme(int[] renkler)
+        {
+            Console.Clear();
+            renkAdi();
+            whileDongu = true;
+            switchDongu = true;
+            while (whileDongu)
+            {
+                tanim = "Eklemek";
+                Console.WriteLine("\nHangi rengi eklemek istiyorsunuz: ?");
+                belirtec = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nKac adet istiyorsunuz: ?");
+                adet = Convert.ToInt32(Console.ReadLine());
+                renkler[belirtec] += adet;
+                switchDongusu();
+            }
+
+            return renkler;
+        }
+
+        public void renkCikarma(int[] renkler)
+        {
+            Console.Clear();
+            renkAdi();
+            whileDongu = true;
+            switchDongu = true;
+            while (whileDongu)
+            {
+                tanim = "Cikarmak";
+                Console.WriteLine("\nHangi rengi cikarmak istiyorsunuz: ?");
+                belirtec = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nKac adet cikarmak istiyorsunuz: ?");
+                adet = Convert.ToInt32(Console.ReadLine());
+                if (renkler[belirtec] - adet < 0)
+                {
+                    Console.WriteLine("\nÇıkarmak istediğiniz adet mevcut adetten fazladır. İşlem gerçekleştirilmemiştir.");
+                }
+                else
+                {
+                    renkler[belirtec] -= adet;
+                }
+
+                switchDongusu();
+            }
+        }
+
+        public void renkArama(int[] renkler)
+        {
+            int toplamUcret = 0;
+            bool renkAlimiVar = false;
+            Console.WriteLine("\n");
+            for (int i = 0; i < renklerAdi.Length; i++)
+            {
+                if (renkler[i] > 0)
+                {
+                    renkAlimiVar = true;
+                    toplamUcret += renkFiyati[i] * renkler[i];
+                    Console.WriteLine("{0} renginden {1} tane aldiniz. Adet Basina Ucreti: {2}₺", renklerAdi[i], renkler[i], renkFiyati[i]);
+                }
+            }
+
+            if (renkAlimiVar == false)
+            {
+                Console.WriteLine("Herhangi bir renk seçmediniz.\n");
+            }
+
+            Console.WriteLine("\nToplam Ucretiniz: {0}₺", toplamUcret);
+        }
+
+        public void switchDongusu()
+        {
+            whileDongu = true;
+            switchDongu = true;
+            while (switchDongu)
+            {
+                Console.WriteLine("\n{0} istediginiz baska renk var mı (1 Evet/ 0 Hayır): ? ", tanim);
+                secenek = Convert.ToInt32(Console.ReadLine());
+                switch (secenek)
+                {
+                    case 0:
+                        whileDongu = false;
+                        switchDongu = false;
+                        continue;
+                    case 1:
+                        switchDongu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Hatalı parametre girdiniz.");
+                        break;
+                }
+            }
+        }
+
+        public void renkAdi()
+        {
+            Console.WriteLine("Renkler sayilara karsilik degerleri verilmistir bunlar sirasiyla;\n\nRenkler ----- Ucret(₺)\n0-Beyaz ----- 36₺/kg \n1-Gri ------- 36₺/kg \n2-K.Rengi --- 40₺/kg \n3-Mavi ------ 37₺/kg\n4-Mor ------- 35₺/kg\n5-Pembe ----- 35₺/kg\n6-Sari ------ 40₺/kg \n7-Siyah ----- 40₺/kg \n8-Yesil ----- 37₺/kg");
+        }
+    }
+}
---
+++ ConsoleApp1/mainProgram.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	public class mainProgram
+	{
+		/* 
+			------------------------------------------------------TANIMLAR------------------------------------------------------
+			
+			---MAIN CLASS'IN ICERSINDEKI;
+			
+			int[] renkler = Kullanıcı girdiği renk adetlerini tutar.
+			
+			int mainSecim = Kullanıcının girdiği menüdeki numarayı tutar.
+			
+			bool mainWhile = Ana program içerisindeki while döngüsünü döndürür. Yani seçenek ekranın durmadan göstermesini sağlar.
+
+			---HESAPLAMA CLASS'IN ICERISINDEKI;
+			
+			string[] renklerAdi = Renkleri menüdeki gibi sıralar. Yani int[] renkler yardımıyla hangisi seçiliyse daha sonrasında
+			dict mantığıyla çalışarak kullanıcıya gösterebilir. Örneğin;
+			renkler[0] = 2 değerinde başka tanımlar dahilinde çıktı şu şekilde olacaktır;
+			Beyaz renginden 2 adet seçilmiştir.
+			
+			int[] renkFiyati = Renklerin dict mantığı gibi ucretlerini gösterir.
+			
+			int belirtec = 0; = Kullanıcının menülerde ekleme,çıkarma veya gösterme gibi durumlarda hangi rengi seçtiğini öğrenmek
+			için kullanılır.
+			
+			int adet = 0; = Kullanıcının seçtiği rengin kaç tane aldığını öğrenmek için kullanılır.
+			
+			int secenek = 0; = Kullanıcı ekleme veya çıkarma işleminden sonra ekstra olarak aynı işlemi yapmak isterse bu komut değerini
+			girerek tekrarlandırmasını sağlar.
+			
+			string tanim = ""; = Yer kazanmak adına "Eklemek" veya "Çıkarmak" kelimesini değiştirerek program çalıştırır.
+			
+			bool whileDongu = true; = Kodlamanın içerisindeki birden çok while döngüsünü kontrol etmek için kullanır.
+			
+			bool switchDongu = true; = Kodlamanın içerisindeki birden çok switch döngüsünü kontrol etmek için kullanır.
+			
+				--- renkarama METODU İÇERİSİNDEKİ
+				
+				int toplamUcret = 0; = Kullanıcı yaptığı seçimler sonucundaki ücreti hesaplar.
+				
+				bool renkAlimiVar = false; = Kullanıcı renk seçimi yapıp yapmadığına bakar.
+			
+			---------------------------------------------HAZIRLAYAN: EMİRCAN DİZDAR---------------------------------------------		
+			
+			DİPNOT = Program dotnetfiddle.net üzerinden yazılmıştır. Başka programlar üzerinde düzenleme yapılması gerekebilir.
+			*/
+
+		public static void Main(string[] args)
+		{
+
+			int[] renkler = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			int mainSecim = 0;
+			bool mainWhile = true;
+			Hesaplama hesaplama = new Hesaplama();
+			Console.WriteLine("Bu Program Renk Secmenizi Saglar.");
+			hesaplama.renkAdi();
+			while (mainWhile)
+			{
+				Console.WriteLine("\nHangi islemi yapmak istiyorsunuz: \n1-Boya Ekleme\n2-Boya Cikarma\n3-Boya Turlerini Gorme\n4-Sectiginiz Boyalar/Adetleri/Toplam Ucret,\n5-Cikis");
+				mainSecim = Convert.ToInt32(Console.ReadLine());
+				switch (mainSecim)
+				{
+					case 1:
+						hesaplama.renkEkleme(renkler);
+						break;
+					case 2:
+						hesaplama.renkCikarma(renkler);
+						break;
+					case 3:
+						Console.Clear();
+						hesaplama.renkAdi();
+						break;
+					case 4:
+						hesaplama.renkArama(renkler);
+						break;
+					case 5:
+						mainWhile = false;
+						Console.WriteLine("Program basariyla sonlanmistir...");
+						break;
+					default:
+						Console.WriteLine("Hatalı secim yaptiniz.");
+						break;
+				}
+			}
+		}
+	}
+}
